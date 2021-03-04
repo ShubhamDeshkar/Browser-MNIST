46dd20cb13e95afa7646d839affd212569a5b35e
Shubham Deshkar <shubham_deshkar@yahoo.com>
2021-03-03 20:23:58 -0600

    change position of function

--- prediction.js
+++ prediction.js
@@ -5,23 +5,6 @@ async function loadModel() {
     return await tf.loadLayersModel('./model.json');
 }
 
-const predict_button = document.getElementById('predict');
-predict_button.addEventListener('click', () => {
-    const image_data = ctx.getImageData(0, 0, 256, 256);
-    const tf_image = tf.browser.fromPixels(image_data, 1);
-    var tf_resized_image = tf.image.resizeBilinear(tf_image, [32, 32]);
-    tf_resized_image = invert_pixel_values(tf_resized_image);
-    tf_resized_image = min_max_scaling(tf_resized_image);
-    tf_resized_image = tf_resized_image.reshape([1, 32, 32, 1]);
-
-    loadModel().then(model => {
-        const predictions = model.predict(tf_resized_image).dataSync();
-        const number = tf.argMax(predictions).dataSync()[0];
-        console.log(number);
-        display_result(number);
-    })
-})
-
 display_result = (number) => {
     document.getElementById('result').innerText = number;
 }
@@ -43,3 +26,20 @@ min_max_scaling = (tensor) => {
     tensor = tensor.add(tf.scalar(offset));
     return tensor;
 }
+
+const predict_button = document.getElementById('predict');
+predict_button.addEventListener('click', () => {
+    const image_data = ctx.getImageData(0, 0, 256, 256);
+    const tf_image = tf.browser.fromPixels(image_data, 1);
+    var tf_resized_image = tf.image.resizeBilinear(tf_image, [32, 32]);
+    tf_resized_image = invert_pixel_values(tf_resized_image);
+    tf_resized_image = min_max_scaling(tf_resized_image);
+    tf_resized_image = tf_resized_image.reshape([1, 32, 32, 1]);
+
+    loadModel().then(model => {
+        const predictions = model.predict(tf_resized_image).dataSync();
+        const number = tf.argMax(predictions).dataSync()[0];
+        console.log(number);
+        display_result(number);
+    })
+})
